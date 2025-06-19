e613663a180580ba1885c91988201da70e3ea2cf
Zipora5572 <z0548545572@gmail.com>
2025-06-19 19:51:18 +0300

    Initial commit

---
+++ src/app/features/tickets/components/ticket-create/ticket-create.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ticket-create',
+  imports: [],
+  templateUrl: './ticket-create.component.html',
+  styleUrl: './ticket-create.component.css'
+})
+export class TicketCreateComponent {
+
+}
